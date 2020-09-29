c78a0990ebbcef05b2817d3587fb51c13da9a921
Marcus Holl <marcus.holl@sap.com>
2020-09-29 14:29:15 +0200

    troubleshoot: more log output inside unstashAll

--- src/com/sap/piper/Utils.groovy
+++ src/com/sap/piper/Utils.groovy
@@ -94,8 +94,12 @@ def unstashAll(stashContent) {
     def unstashedContent = []
     if (stashContent) {
         for (i = 0; i < stashContent.size(); i++) {
+            echo "[MH] handling stash content: ${stashContent[i]}"
             if (stashContent[i]) {
+                echo "[MH] unstashing ${stashContent[i]}"
                 unstashedContent += unstash(stashContent[i])
+            } else {
+                echo "[MH] skiped unstashing ${stashContent[i]}"
             }
         }
     } else {
